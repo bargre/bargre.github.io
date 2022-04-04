233e85e61e69a5f2af1f7d958e377b08b6c0f9b4
bargre <bargre@naver.com>
2022-04-04 16:42:44 +0900

    resize() 추가

--- docs/link/testTwo/js/main.js
+++ docs/link/testTwo/js/main.js
@@ -1,65 +1,71 @@
 $(function() {
 
-
-
-//resize header
-  $(window).on('load resize', function() {
+  //resize header
+  $(window).resize(function() {
 
     const windowWidth = $(window).width();
-  // console.log(windowWidth);
-  if(windowWidth <= 1080) {
-    $(".gnb-tit").click(function() {
-       $(this).parent(".gnb-list").toggleClass("on");
+    // console.log(windowWidth);
+    if (windowWidth <= 1080) {
+      $(".gnb-tit").click(function() {
+        $(this).parent(".gnb-list").toggleClass("on");
+      });
+    }
+
+    $(".gnb-area").mouseenter(function() { //pc mouse hover
+      $(".headerContainer").addClass("on");
+      $(".search-area").addClass("on");
     });
-        }
-        
-        $(".gnb-area").mouseenter(function() { //pc mouse hover
-          $(".headerContainer").addClass("on");
-          $(".search-area").addClass("on");
-        });
 
-        $(".gnb-area").mouseleave(function() {
-          $(".headerContainer").removeClass("on");
-            $(".search-area").removeClass("on");
-        });
+    $(".gnb-area").mouseleave(function() {
+      $(".headerContainer").removeClass("on");
+      $(".search-area").removeClass("on");
+    });
 
+  }).resize(); //resize
 
+  //메뉴버튼 열기
+  $(".menu-btn").click(function() {
+    $(".back-bg").css({
+      display: "block"
+    });
+    $(".gnbWrap").stop().animate({
+      left: "0px"
+    }, 200);
+    $(".search-area").addClass("on"); //지점찾기 z-index
+  });
+  //매뉴버튼 닫기
+  $(".back-bg").click(function() {
+    $(".back-bg").css({
+      display: "none"
+    });
+    $(".gnbWrap").stop().animate({
+      left: "-400px"
+    }, 200);
+    $(".gnb-list").removeClass("on"); //리스트 전체 닫기
+    $(".search-area").removeClass("on"); //지점찾기 z-index
+  });
 
-  });//resize
-
-//메뉴버튼 열기
-$(".menu-btn").click(function(){
-  $(".back-bg").css({display:"block"});
-    $(".gnbWrap").stop().animate({left:"0px"}, 200);
-     $(".search-area").addClass("on"); //지점찾기 z-index
-});
-//매뉴버튼 닫기
- $(".back-bg").click(function(){
-   $(".back-bg").css({display:"none"});
-     $(".gnbWrap").stop().animate({left:"-400px"}, 200);
-       $(".gnb-list").removeClass("on");//리스트 전체 닫기
-     $(".search-area").removeClass("on");//지점찾기 z-index
- });
-
- // 닫기 이벤트 - 닫기버튼 클래스 : "close-btn" / 닫히는 영역 클래스 :"close"
-  $(".close-btn").click(function(){
-    $(".close").css({display:"none"});
+  // 닫기 이벤트 - 닫기버튼 클래스 : "close-btn" / 닫히는 영역 클래스 :"close"
+  $(".close-btn").click(function() {
+    $(".close").css({
+      display: "none"
+    });
   });
 
   //header 스크롤 이벤트
-  $(window).scroll(function(){
-    var _ht =  $(window).scrollTop();
-    if(_ht > 0){
+  $(window).scroll(function() {
+    var _ht = $(window).scrollTop();
+    if (_ht > 0) {
       $(".headerContainer").addClass("scroll");
-    }else {
-        $(".headerContainer").removeClass("scroll");
+    } else {
+      $(".headerContainer").removeClass("scroll");
     }
   });
 
   //지점찾기
-$(".btn-agency").click(function(){
-  $(".search-agency-area").toggleClass("on");
-  $(this).toggleClass("on");
-});
+  $(".btn-agency").click(function() {
+    $(".search-agency-area").toggleClass("on");
+    $(this).toggleClass("on");
+  });
 
 }); //end
