293f851c1d7f04da7003ecbca87eff8645e63fb2
bargre <bargre@naver.com>
2022-06-22 17:04:11 +0900

    project page publishing

--- docs/portfolio/js/link.js
+++ docs/portfolio/js/link.js
@@ -8,15 +8,15 @@ $(document).ready(function(){
     if(width <= 1140){
       $(".design-list-area li").eq(0).click(function(e){
         e.stopPropagation();
-        window.open("link/innisfree_re/mobile/index.html", "a", "width=400, height=600, left=200, top=50");
+        window.open("../link/innisfree_re/mobile/index.html", "a", "width=400, height=600, left=200, top=50");
         });
       $(".design-list-area li").eq(1).click(function(e){
         e.stopPropagation();
-        window.open("link/audi_re/index.html", "a", "width=400, height=600, left=200, top=50");
+        window.open("../link/audi_re/index.html", "a", "width=400, height=600, left=200, top=50");
       });
       $(".design-list-area li").eq(2).click(function(e){
         e.stopPropagation();
-        window.open("link/MyPlant/index.html", "a", "width=375, height=650, left=200, top=50");
+        window.open("../link/MyPlant/index.html", "a", "width=375, height=650, left=200, top=50");
       });
     } else {
       $(".design-list-area li").eq(0).off("click");
@@ -30,23 +30,23 @@ $(document).ready(function(){
 
 //이니스프리 사이트 오픈
   $("#inniPcOpen").click(function(){
-    window.open("link/innisfree_re/index.html");
+    window.open("../link/innisfree_re/index.html");
   });
 //이니스프리 모바일 사이트 오픈
   $("#inniMobileOpen").click(function(){
-    window.open("link/innisfree_re/mobile/index.html", "a", "width=375, height=650, left=200, top=50");
+    window.open("../link/innisfree_re/mobile/index.html", "a", "width=375, height=650, left=200, top=50");
   });
 //아우디 사이트 오픈
   $("#audiPcOpen").click(function(){
-    window.open("link/audi_re/index.html");
+    window.open("../link/audi_re/index.html");
   });
 //아우디 모바일 사이트 오픈
   $("#audiMobileOpen").click(function(){
-    window.open("link/audi_re/index.html", "a", "width=375, height=650, left=200, top=50");
+    window.open("../link/audi_re/index.html", "a", "width=375, height=650, left=200, top=50");
   });
   //반려식물 모바일 사이트 오픈
     $("#plantMobileOpen").click(function(){
-      window.open("link/MyPlant/index.html", "a", "width=375, height=650, left=200, top=50");
+      window.open("../link/MyPlant/index.html", "a", "width=375, height=650, left=200, top=50");
     });
 
 
