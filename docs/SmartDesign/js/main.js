ac93b8836b7704515e986df31d5fd7a85b0f55e7
bargre <81691081+bargre@users.noreply.github.com>
2022-11-03 15:11:52 +0900

    스디팀 포폴 최초 업데이트

---
+++ docs/SmartDesign/js/main.js
@@ -0,0 +1,29 @@
+
+$(function(){
+
+    //모바일 메뉴 닫기
+ $(".btn-menu-close").click(function(){
+ $(".nav").removeClass("active");
+ });
+ //모바일 메뉴 열기
+ $(".menu-area").click(function(){
+    $(".nav").addClass("active");
+ });
+
+
+ //section 2 이미지 스크롤 이벤트
+$(window).scroll(function(){
+    AOS.init();
+ const doc = $(document).scrollTop();
+ const img = $(".img-box").offset().top - 500 ;
+//console.log(doc);
+
+if(doc > img){
+    $(".img-box").addClass("active");
+} else {
+    $(".img-box").removeClass("active");
+}
+});//scroll
+
+
+}); //end
