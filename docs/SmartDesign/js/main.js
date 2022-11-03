e30593c9a4f8cec125fe88751687c1f6740f72b5
bargre <81691081+bargre@users.noreply.github.com>
2022-11-03 17:47:14 +0900

    메인 완료

--- docs/SmartDesign/js/main.js
+++ docs/SmartDesign/js/main.js
@@ -1,29 +1,36 @@
 
-$(function(){
+$(function () {
 
     //모바일 메뉴 닫기
- $(".btn-menu-close").click(function(){
- $(".nav").removeClass("active");
- });
- //모바일 메뉴 열기
- $(".menu-area").click(function(){
-    $(".nav").addClass("active");
- });
+    $(".btn-menu-close").click(function () {
+        $(".nav").removeClass("active");
+    });
+    //모바일 메뉴 열기
+    $(".menu-area").click(function () {
+        $(".nav").addClass("active");
+    });
 
+    //section 2 이미지 스크롤 이벤트
 
- //section 2 이미지 스크롤 이벤트
-$(window).scroll(function(){
-    AOS.init();
- const doc = $(document).scrollTop();
- const img = $(".img-box").offset().top - 500 ;
-//console.log(doc);
+    $(window).scroll(function () {
+        AOS.init();
 
-if(doc > img){
-    $(".img-box").addClass("active");
-} else {
-    $(".img-box").removeClass("active");
-}
-});//scroll
+        const doc = $(document).scrollTop();
+        const img = $(".img-box").offset().top - 500;
+        //console.log(doc);  
+
+        if (doc > img) {
+            $(".img-box").addClass("active");
+        } else {
+            $(".img-box").removeClass("active");
+        }
+
+        if (doc > 0) {
+            $(".header").addClass("scroll");
+        } else {
+            $(".header").removeClass("scroll");
+        }
+    });//scroll
 
 
 }); //end
