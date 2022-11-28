61029ea46ca6abaa2ff0fbf29e64e97c933ba5ac
bargre <81691081+bargre@users.noreply.github.com>
2022-11-28 17:02:18 +0900

    포폴 완료 1차 업로드

--- docs/SmartDesign/js/main.js
+++ docs/SmartDesign/js/main.js
@@ -1,36 +1,93 @@
 
 $(function () {
 
-    //모바일 메뉴 닫기
-    $(".btn-menu-close").click(function () {
-        $(".nav").removeClass("active");
+  //모바일 메뉴 닫기
+  $(".btn-menu-close").click(function () {
+    $(".nav").removeClass("active");
+  });
+  //모바일 메뉴 열기
+  $(".menu-area").click(function () {
+    $(".nav").addClass("active");
+  });
+
+  //section 2 이미지 스크롤 이벤트
+  //header scroll -> index만 적용
+
+  $(window).scroll(function () {
+    AOS.init();
+    const doc = $(document).scrollTop();
+    if ($("body").hasClass("main")) {
+
+      if (doc > 100) {
+        $(".header").addClass("scroll");
+        $(".header").removeClass("top");
+      } else {
+        $(".header").removeClass("scroll");
+        $(".header").addClass("top");
+      }
+
+      //section2 img motion
+      const img = $(".img-box").offset().top - 700;
+
+      if (doc > img) {
+        $(".img-box").addClass("active");
+      } else {
+        $(".img-box").removeClass("active");
+      }
+    } else {
+      //sub header scroll
+      if (doc > 0) {
+        $(".header").addClass("scroll");
+      } else {
+        $(".header").removeClass("scroll");
+      }
+    }
+
+    if(doc > 200){
+      $(".top-button-area").show();
+    } else {
+      $(".top-button-area").hide();
+    }
+  });//scroll
+
+
+  // * works tab
+  figureBlock();
+  $(".filter-links li a").click(function () {
+
+    figureNone();
+
+    if ($(this).hasClass("link-all")) {
+      $(".filter-links li a").removeClass("active");
+      figureBlock();
+      $(".link-all").addClass("active");
+    } else if ($(this).hasClass("link-web")) {
+      $(".filter-links li a").removeClass("active");
+      $(".filter-sections .web").addClass("view");
+      $(this).addClass("active");
+    } else if ($(this).hasClass("link-app")) {
+      $(".filter-links li a").removeClass("active");
+      $(".filter-sections .app").addClass("view");
+      $(this).addClass("active");
+    } else if ($(this).hasClass("link-ecom")) {
+      $(".filter-links li a").removeClass("active");
+      $(".filter-sections .ecommerce").addClass("view");
+      $(this).addClass("active");
+    }
+
+    return false;
+  });
+
+  function figureBlock() {
+    $(".filter-sections figure").addClass("view");
+  }
+  function figureNone() {
+    $(".filter-sections figure").removeClass("view");
+  }
+
+
+    $(".work-info").click(function(){
+      $(this).toggleClass("active");
     });
-    //모바일 메뉴 열기
-    $(".menu-area").click(function () {
-        $(".nav").addClass("active");
-    });
-
-    //section 2 이미지 스크롤 이벤트
-
-    $(window).scroll(function () {
-        AOS.init();
-
-        const doc = $(document).scrollTop();
-        const img = $(".img-box").offset().top - 500;
-        //console.log(doc);  
-
-        if (doc > img) {
-            $(".img-box").addClass("active");
-        } else {
-            $(".img-box").removeClass("active");
-        }
-
-        if (doc > 0) {
-            $(".header").addClass("scroll");
-        } else {
-            $(".header").removeClass("scroll");
-        }
-    });//scroll
-
-
+ 
 }); //end
